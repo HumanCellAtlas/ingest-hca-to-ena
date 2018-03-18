415b60b82923af0347dd6e61c1e9a6ef466eec2c
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-03-18 08:16:20 +0000

    initial test

---
+++ README.md
@@ -0,0 +1,5 @@
+## Run Locally
+
+```
+serverless invoke local --function convert --path ./examples/metadata_spleen_v5_20180313_userFriendlyHeaders.json
+```
--- test_handler.py
+++ test_handler.py
@@ -5,9 +5,9 @@ from handler import convert
 class TestHandler(unittest.TestCase):
 
     def test_convert(self):
-        res = convert(None, None)
-        self.assertEquals(200, res['statusCode'])
-        self.assertTrue(len(res['body']) > 0)
+        with open('examples/metadata_spleen_v5_20180313_userFriendlyHeaders.json') as json_data:
+            dataset_json = json.load(json_data)
+            convert(dataset_json)
 
 if __name__ == '__main__':
     unittest.main()
