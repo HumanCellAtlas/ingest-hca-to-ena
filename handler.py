cc1283059b8f280153326441fadf7fc5a563a484
Daniel Vaughan <dan@danielvaughan.com>
2018-03-18 09:09:24 +0000

    PEP8

--- handler.py
+++ handler.py
@@ -1,19 +1,21 @@
 import json
 
+
 def handle(event, context):
-    submission = _process_event(event)
+    _process_event(event)
     response = {
         "statusCode": 200,
         "body": {}
     }
     return response
-    
+
+
 def convert(dataset_json):
     for element in dataset_json:
         if 'schema_type' in element:
             print(element['schema_type'])
-    
+
+
 def _process_event(event):
     submission = json.loads(event["body"][0])
     return submission
-    
--- test_handler.py
+++ test_handler.py
@@ -2,12 +2,14 @@ import unittest
 import json
 from handler import convert
 
+
 class TestHandler(unittest.TestCase):
 
     def test_convert(self):
         with open('examples/metadata_spleen_v5_20180313_userFriendlyHeaders.json') as json_data:
             dataset_json = json.load(json_data)
             convert(dataset_json)
+
 
 if __name__ == '__main__':
     unittest.main()
