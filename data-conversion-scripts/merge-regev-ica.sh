554f82cd39bd11f2499e097be513c77089207aba
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-04-16 10:20:30 +0100

    Added alternative shell script.

--- data-conversion-scripts/merge-regev-ica.sh
+++ data-conversion-scripts/merge-regev-ica.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 cd /data/Regev-ICA/data/bone-marrow/fastqs
 
+
+# ls MantonBM4_*L001*I1*.fastq.gz | xargs cat >> MantonBM4_HiSeq_L001_I1_001.fastq.gz
 cat MantonBM4_HiSeq_1_S25_L001_I1_001.fastq.gz MantonBM4_HiSeq_2_S26_L001_I1_001.fastq.gz MantonBM4_HiSeq_3_S27_L001_I1_001.fastq.gz MantonBM4_HiSeq_4_S28_L001_I1_001.fastq.gz MantonBM4_HiSeq_5_S29_L001_I1_001.fastq.gz MantonBM4_HiSeq_6_S30_L001_I1_001.fastq.gz MantonBM4_HiSeq_7_S31_L001_I1_001.fastq.gz MantonBM4_HiSeq_8_S32_L001_I1_001.fastq.gz > ../fastq-concat/MantonBM4_HiSeq_L001_I1_001.fastq.gz
 
+# ls MantonBM4_*L001*R1*.fastq.gz | xargs cat >> MantonBM4_HiSeq_L001_R1_001.fastq.gz
 cat MantonBM4_HiSeq_1_S25_L001_R1_001.fastq.gz MantonBM4_HiSeq_2_S26_L001_R1_001.fastq.gz MantonBM4_HiSeq_3_S27_L001_R1_001.fastq.gz MantonBM4_HiSeq_4_S28_L001_R1_001.fastq.gz MantonBM4_HiSeq_5_S29_L001_R1_001.fastq.gz MantonBM4_HiSeq_6_S30_L001_R1_001.fastq.gz MantonBM4_HiSeq_7_S31_L001_R1_001.fastq.gz MantonBM4_HiSeq_8_S32_L001_R1_001.fastq.gz > ../fastq-concat/MantonBM4_HiSeq_L001_R1_001.fastq.gz
 
 cat MantonBM4_HiSeq_1_S25_L001_R2_001.fastq.gz MantonBM4_HiSeq_2_S26_L001_R2_001.fastq.gz MantonBM4_HiSeq_3_S27_L001_R2_001.fastq.gz MantonBM4_HiSeq_4_S28_L001_R2_001.fastq.gz MantonBM4_HiSeq_5_S29_L001_R2_001.fastq.gz MantonBM4_HiSeq_6_S30_L001_R2_001.fastq.gz MantonBM4_HiSeq_7_S31_L001_R2_001.fastq.gz MantonBM4_HiSeq_8_S32_L001_R2_001.fastq.gz > ../fastq-concat/MantonBM4_HiSeq_L001_R2_001.fastq.gz
